67e5a18476ac0436382117e2c64d712468ec8fca
John Cook <hellojohncook@gmail.com>
2022-02-20 23:42:11 -0800

    Preloading the watermelon IMG

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var launcher, launcherImg;
 
+function preload() {
+  launcherImg = loadImage('Photos/Watermelon.png');
+}
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   launcher = createSprite(100, 650, 50, 50);
-  console.log(windowWidth,windowHeight)
+  console.log(windowWidth,windowHeight);
 }
 
 function draw() {
