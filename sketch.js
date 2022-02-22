383e122c32cf3734ef99cbf83042c5cc298a10e6
John Cook <hellojohncook@gmail.com>
2022-02-22 02:07:34 -0800

    Making some edits

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-//All of the global variables
-var launcher, launcherImg;
 //Matter.js stuff
 const Engine = Matter.Engine
 const  World = Matter.World
 const  Bodies = Matter.Bodies;
 const Body = Matter.Body
+
+//All of the global variables
+var launcher, launcherImg;
+var melon;
 
 function preload() {
 //Adding the pictures to the sprites
@@ -14,15 +16,27 @@ function preload() {
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
+//Setting up the world
+  engine = Engine.create();
+  world = engine.world;
+
 //Setting up the launher
   launcher = createSprite(100, 650, 50, 50);
   launcher.addImage(launcherImg)
   launcher.scale = 0.1
+
+  melon = new Melon()
 
   console.log(windowWidth,windowHeight);
 }
 
 function draw() {
   background(0);  
+
+  if (keyDown(k)){
+
+
+  }
+
   drawSprites();
 }
